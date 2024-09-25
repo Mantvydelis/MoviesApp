6e47085dee42fedd87cc618cb44b5319618adb21
Mantvydas Šemeta <mantvydassemeta@gmail.com>
2024-09-25 15:33:58 +0300

    Add project files.

---
+++ MoviesApp/Controllers/TmdbController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesApp.Services;
+
+namespace MoviesApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TmdbController : ControllerBase
+    {
+        private readonly TmdbService _tmdbService;
+
+        public TmdbController(TmdbService tmdbService)
+        {
+            _tmdbService = tmdbService;
+        }
+
+        [HttpGet("authenticate")]
+        public async Task<IActionResult> Authenticate()
+        {
+            var result = await _tmdbService.AuthenticateAsync();
+            return Ok(result); // Return the TMDb API response
+        }
+    }
+}
---
+++ MoviesApp/Services/TmdbService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Headers;
+
+namespace MoviesApp.Services
+{
+    public class TmdbService
+    {
+        private readonly HttpClient _client;
+        private const string BaseUrl = "https://api.themoviedb.org/3";
+        private const string ApiKey = "377f2c6b1dc000572573b6578e0497d4";
+
+        public TmdbService(HttpClient client)
+        {
+            _client = client;
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ApiKey);
+        }
+
+        public async Task<string> AuthenticateAsync()
+        {
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"{BaseUrl}/authentication/token/new")
+            };
+
+            using (var response = await _client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+
+    }
+}
